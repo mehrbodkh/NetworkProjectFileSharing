34feb9fe9dc85d42cdcb77bc91af00a17b1fb101
mehrbod <mehrbod.kh@gmail.com>
2018-01-17 15:45:56 +0330

    feat(core): Adds server
    related master

---
+++ src/Server/Server.java
@@ -0,0 +1,18 @@
+package Server;
+
+/**
+ * created by Mehrbod on 1.17.2018
+ * manages the server
+ */
+public class Server {
+    //
+    // server port
+    //
+    private final int SERVER_PORT = 47021;
+
+    //
+    // server info
+    //
+    private String ip;
+
+}
